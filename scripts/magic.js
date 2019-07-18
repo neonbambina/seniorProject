d5bd4c16d7e2cb44761d43e7300b0666ac7aec14
Ariel George <arielgeorge@205-185-99-87.directcom.com>
2019-07-18 09:57:44 -0600

    a whole programmer

--- scripts/magic.js
+++ scripts/magic.js
@@ -2,7 +2,7 @@ var drop = document.getElementById('photography');
 var catagories = document.getElementById('photography__catagories');
 
  function toggleMenu(){
-   catagories.classList.toggle('hidden');
+   catagories.classList.toggle('shown');
 };
 
 drop.addEventListener("click", toggleMenu, false);
